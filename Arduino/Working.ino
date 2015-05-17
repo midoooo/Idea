5efb20fef72281a0d144cae8d17e8fa24ff059c9
prabaprakash <prabaprakash@outlook.com>
2015-05-17 18:42:24 +0530

    new

--- Arduino/Working.ino
+++ Arduino/Working.ino
@@ -1,19 +1,10 @@
-/*
-  Example Bluetooth Serial Passthrough Sketch
- by: Jim Lindblom
- SparkFun Electronics
- date: February 26, 2013
- license: Public domain
- 
- This example sketch converts an RN-42 bluetooth module to
- communicate at 9600 bps (from 115200), and passes any serial
- data between Serial Monitor and bluetooth module.
- */
+
 #include <SoftwareSerial.h>  
 
 int bluetoothTx = 1;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 0;  // RX-I pin of bluetooth mate, Arduino D3
 int ledpin=13;
+int ledpin2=14;
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 char a;
 void setup()
@@ -24,6 +15,7 @@ void setup()
   bluetooth.begin(9600);  // Start bluetooth serial at 9600
   bluetooth.println("Bluetooth On please press 1 or 0 blink LED ..");
   pinMode(ledpin,OUTPUT);
+   pinMode(ledpin2,OUTPUT);
 }
 
 void loop()
@@ -32,17 +24,26 @@ void loop()
   { 
     // Send any characters the bluetooth prints to the serial monitor
     a=bluetooth.read();
+    if(a=='3')
+    {
+        digitalWrite(ledpin2,1);
+    }
+    else if(a=='4')
+    {
+      digitalWrite(ledpin2,0);
+    }
+
     if(a=='1')
     {
         digitalWrite(ledpin,1);
     }
-    else if(a=='0')
+    else if(a=='2')
     {
       digitalWrite(ledpin,0);
     }
     else
     {
-      bluetooth.write("Press 0 to switch off");
+      bluetooth.write("Press 1,3 to switch on and 2,4 to off");
     }
     
   }
